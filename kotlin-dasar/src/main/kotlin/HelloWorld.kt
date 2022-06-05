5a961858041babd9375fa3b31dd46ade93d176b9
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-05 20:00:55 +0700

    Hello, World

---
+++ kotlin-dasar/src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Hello World!")
+
+    println("Nanda Iqbal Hanafi")
+
+    // kalo print doang, tidak menambah enter
+    print("Nanda")
+    print(" Iqbal")
+    print(" Hanafi")
+}
