0ee3762ee07c83b41908555053523915a93bc6e7
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-06 23:45:20 +0700

    Character

---
+++ kotlin-dasar/src/main/kotlin/Character.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+
+    // char dalam tanda petik satu -> hanya untuk menyimpan satu nilai character
+    var n: Char = 'N'
+    var a: Char = 'A'
+    var d: Char = 'D'
+    var satu: Char = '1'
+
+    print(n)
+    print(a)
+    print(n)
+    print(d)
+    print(a)
+    print(satu)
+
+}
