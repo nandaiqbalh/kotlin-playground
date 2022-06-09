46d74a855f7a94eaec6cd0b27c38960fc645daa4
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-09 21:59:22 +0700

    Percabangan

---
+++ kotlin-dasar/src/main/kotlin/13_IfExpression.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val  finalExam = 60
+
+    if (finalExam >= 76) {
+        // jika kondisi true, maka blok ini akan dieksekusi
+        println("Selamat, anda lulus")
+    } else if (finalExam >= 60){
+
+        println("Lumayan lahh")
+    } else {
+        // jika kondisi false maka ini yang akan dieksekusi
+        println("Maaf, anda tidak lulus")
+    }
+
+    println("\nIni akhir program")
+}
