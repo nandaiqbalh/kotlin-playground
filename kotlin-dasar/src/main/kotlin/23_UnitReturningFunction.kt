badc242b53aa1a38f8035167ca05a59622d7a44f
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-11 18:18:32 +0700

    Function Returning Type

--- kotlin-dasar/src/main/kotlin/23_UnitReturningFunction.kt
+++ kotlin-dasar/src/main/kotlin/23_UnitReturningFunction.kt
@@ -3,7 +3,6 @@ fun main() {
     printHello(null)
 
 }
-
 // penggunaan kata Unit tidaklah wajib
 fun printHello(name: String?) :Unit{
 
---
+++ kotlin-dasar/src/main/kotlin/24_FunctionReturnType.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    println(sumNilai(10, 10))
+}
+
+fun sumNilai(a: Int, b:Int) :Int{
+    val total = a + b
+    return total
+}
