7029f8451abc61648dbce8fd3a0802779cc66bfb
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-17 19:25:44 +0700

    Closure

---
+++ kotlin-dasar/src/main/kotlin/36_Closure.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var counter: Int = 0
+    val lambdaIncrement: () -> Unit = {
+        println("Increment")
+        counter++
+    }
+
+    lambdaIncrement()
+    lambdaIncrement()
+}
