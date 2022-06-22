bd544b9c46dfd54c9a1be8133d32db3dc565305b
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-22 22:41:25 +0700

    Properties di constructor

--- kotlin-oop/src/main/kotlin/app/Contructor.kt
+++ kotlin-oop/src/main/kotlin/app/Contructor.kt
@@ -2,6 +2,7 @@ package app
 
 import data.Car
 import data.Person
+import data.User
 
 fun main() {
     val toyota = Car("Minibus", 4)
@@ -10,4 +11,12 @@ fun main() {
 
     val person = Person("Nanda", null, "Iqbal Hanafi")
     println(person.lastName)
+
+    // properties di constructor
+    val user1 = User("nandaiqbalh", "rahasia123")
+
+    user1.username = "nandaiqbalh123"
+    user1.password = "gajadirahasia"
+
+    println("\nUser ini adalah ${user1.username} yang punya password ${user1.password}")
 }
