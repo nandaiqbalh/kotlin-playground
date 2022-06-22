d5a40702f6e291d6908d4f07e920708dc142f48d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-22 20:56:18 +0700

    Label

---
+++ kotlin-dasar/src/main/kotlin/38_Label.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    loopI@ for(i in 1..10){
+        loopJ@ for (j in 1 .. 10){
+
+            if (i > 5 ){
+                break@loopI
+            }
+            println("$i * $j = ${i * j}")
+
+        }
+    }
+
+}
