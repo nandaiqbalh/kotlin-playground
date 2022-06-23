cd802481c0dfe23689f9ca4d1283ae0a43dafa78
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-23 10:35:06 +0700

    Overloading Function

--- kotlin-oop/src/main/kotlin/app/Function.kt
+++ kotlin-oop/src/main/kotlin/app/Function.kt
@@ -8,4 +8,9 @@ fun main() {
     val student1 = Student("Nanda")
     student1.helloStudent("Bruno")
     student1.runAyo()
+
+    // overloading function
+    val student2 = Student("Nanda Iqbal Hanafi")
+    student2.sayHelloStudent("Bruno Fernandes")
+    student2.sayHelloStudent("Luke", "Shaw")
 }
--- kotlin-oop/src/main/kotlin/data/Student.kt
+++ kotlin-oop/src/main/kotlin/data/Student.kt
@@ -9,4 +9,13 @@ class Student(var name: String) {
     fun runAyo(){
         println("Ayo berlari")
     }
+
+    // overloading -> function yang namanya sama -> parameternya harus beda (baik dari jumlah atau tipenya)\
+    fun sayHelloStudent(yourName: String){
+        println("Hello ${yourName}, my name is ${name}")
+    }
+
+    fun sayHelloStudent(firstName: String, lastName: String){
+        println("Hello ${firstName} ${lastName}, my name is ${name}")
+    }
 }
