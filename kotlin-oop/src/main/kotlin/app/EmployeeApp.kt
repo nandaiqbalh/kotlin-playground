e339cf0d9df4c399a81d2aaac497ad568e29e4b8
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-23 17:14:59 +0700

    Overriding Function

--- kotlin-oop/src/main/kotlin/app/EmployeeApp.kt
+++ kotlin-oop/src/main/kotlin/app/EmployeeApp.kt
@@ -12,4 +12,13 @@ fun main() {
     val president1 = President("Nanda")
     president1.sayHelloeEmployee("Presiden")
 
+    val karyawan1 = Karyawan("Nanda Iqbal Hanafi")
+    karyawan1.helloUntukOverride("Bruno")
+}
+
+final class Karyawan(name: String) : Employee(name){
+
+    override fun helloUntukOverride(name: String) {
+        println("Hello ${name}, my name is ${this.name}. Ini dari overriding function")
+    }
 }
--- kotlin-oop/src/main/kotlin/data/Employee.kt
+++ kotlin-oop/src/main/kotlin/data/Employee.kt
@@ -5,4 +5,9 @@ open class Employee(val name:String) {
     fun sayHelloeEmployee(name: String){
         println("Hello $name, my name is ${this.name}")
     }
+
+    // overriding function
+    open fun helloUntukOverride(name: String){
+        println("Hello ${name}, my name is ${this.name}")
+    }
 }
